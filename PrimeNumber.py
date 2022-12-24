449f62ac3d751fe346224ab820373428ef969e92
ShaikArshad1234 <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-24 01:19:13 -0800

    Add files via upload
    PrimeNumber.

---
+++ PrimeNumber.py
@@ -0,0 +1,20 @@
+
+import math
+def PrimeNumber(num):
+    if num<1:
+        return 0
+    if num==1 or num==2 or num==3:
+        return num
+    count=int(math.sqrt(num))+1
+    for i in range(2,count):
+        if num%i==0:
+            return 0
+    return num
+start=int(input())
+stop=int(input())
+lst=[]
+for i in range(start,stop+1):
+    if PrimeNumber(i):
+        lst.append(i)
+print(*lst)
+print(len(lst))
