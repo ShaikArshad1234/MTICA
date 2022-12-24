6bf49cd3f1ce1a91d6b3ba05e1f70630b0dad6d2
ShaikArshad1234 <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-24 02:31:56 -0800

    Add files via upload
    Interchange program.

---
+++ Interchange program.py
@@ -0,0 +1,9 @@
+
+def interchange3and5(n):
+     n=str(n)
+     n=n.replace('3','.')
+     n=n.replace('5','3')
+     n=n.replace('.','5')
+     return n
+inp=int(input())
+print(interchange3and5(inp))
