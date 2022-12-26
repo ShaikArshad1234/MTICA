1926bfbb6a8483237c4467068f3212b94be2b68f
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-26 00:36:39 -0800

    Extract_Vowels program.
    Extract_Vowels program

---
+++ pp-13-Extract_vowel.py
@@ -0,0 +1,10 @@
+def extract_vowel(s):
+    temp_vowel=' '
+    for i in s:
+        if i in 'AEIOUaeiou':
+            temp_vowel+=i
+    return temp_vowel
+str1=input("Enter the string:")
+#str1=input()
+a=extract_vowel(str1)
+print("vowels:",a)
