f3c38580e3ac44ba7f5820040422f586a3d542c8
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-26 00:34:28 -0800

    Add files via upload

---
+++ pp-11.py
@@ -0,0 +1,6 @@
+s1='"Hi Everyone",I said."How are You ?"'
+s2='"Hi Naveen", I said.'"How are You?"
+#s3='"Hi Mom",I said.'"How are you?"'
+s4="""'Hi mom",I said.'"How are you?"'"""
+#s5=""I want to be a lion tamer!"'
+s6="\"Is this a cheese shp?\"\n\t'Yes'\n\t\"we have all kinds!\""
