3e4330949071ba1ab26c5731d3af685b80a9e13e
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-27 03:11:17 -0800

    Time comprehension

---
+++ p-05-Time.py
@@ -0,0 +1,8 @@
+import time
+inpNo=int(input("Enter a no:"))
+start=time.time()
+for i in range(inpNo):
+    print("i=",i,"i^2=",i*i)
+print("Time taken by loop:",
+      (time.time()-start)*100000)
+ 
