a57b1a9c3f25326b3ef52f6afe66698b568e3106
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-30 03:29:41 -0800

    MemberShip Operator01

---
+++ identical membership operators01.py
@@ -0,0 +1,13 @@
+
+message='Hello World'
+dict={1:'a', 2:'b'}
+print('H' in message)
+print('hello' not in message)
+print(1 in dict)
+print('a' in dict)
+
+lst=[10,20,30,'c','java','python']
+print('java' in lst)
+print('R' in lst)
+print('r' not in lst)
+     
