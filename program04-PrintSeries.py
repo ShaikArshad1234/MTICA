eab2115c2eed5f2ec233ca6b41f08fae211c515b
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-30 03:24:20 -0800

    printSeries1

---
+++ program04-PrintSeries.py
@@ -0,0 +1,18 @@
+def  printSeries(n):
+    for i in range(1,n+1):
+        num=1
+        print()
+        for j in range(i):
+            print(num,end=' ')
+            num+=1
+            
+    return None
+n=int(input())
+printSeries(n)
+
+
+
+    
+
+
+
