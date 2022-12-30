aa2e009c7457623e44cd61e2434c250d5071e45a
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-30 03:27:34 -0800

    printSeriesIncreasing

---
+++ program01-PrintSeriesIncreasing.py
@@ -0,0 +1,10 @@
+def  printSeries(n):
+    num=1
+    for i in range(1,n+1):
+        print()
+        for j in range(i):
+            print(num,end=' ')
+            num+=1
+    return None
+
+    
