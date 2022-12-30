e269d22cbd8e73262893b12a0bd73fecb1a856bc
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-30 03:19:55 -0800

    operators

---
+++ program07-operator.py
@@ -0,0 +1,8 @@
+x=10
+y=4
+print('x=',x,'y=',y,'x&y=',x&y)
+print('x=',x,'y=',y,'x|y=',x|y)
+print('x=',x,'~x=',~x)
+print('x=',x,'y=',y,'x^y=',x^y)
+print('x=',x,'x>>2=',x>>2)
+print('x=',x,'x<<2=',x<<2)
