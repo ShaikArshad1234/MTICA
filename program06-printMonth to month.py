efccf786d2693a703bbbadade2fb433212de3783
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-30 03:22:42 -0800

    printMonth to Month

---
+++ program06-printMonth to month.py
@@ -0,0 +1,65 @@
+def printMonth(dn):
+    mn='Invalid'
+    if(dn==1):
+        mn="January"
+    elif(dn==2):
+        mn= "February"
+    elif(dn==3):
+        mn="march"
+    elif(dn==4):
+        mn= "April"
+    elif(dn==5):
+        mn= "May"
+    elif(dn==6):
+        mn= "June"
+    elif(dn==7):
+        mn= "July"
+    elif(dn==8):
+        mn= "August"
+    elif(dn==9):
+        mn= "September"
+    elif(dn==10):
+        mn= "October"
+    elif(dn==11):
+        mn= "November"
+    elif(dn==12):
+        mn="December"
+    return mn
+def printMonth1(dn):
+    mn='Invalid '
+    if(dn==1):
+        mn="January"
+    if(dn==2):
+        mn= "February"
+    if(dn==3):
+        mn="march"
+    if(dn==4):
+        mn= "April"
+    if(dn==5):
+        mn= "May"
+    if(dn==6):
+        mn= "June"
+    if(dn==7):
+        mn= "July"
+    if(dn==8):
+        mn= "August"
+    if(dn==9):
+        mn= "September"
+    if(dn==10):
+        mn= "October"
+    if(dn==11):
+        mn= "November"
+    if(dn==12):
+        mn="December"
+    return mn
+import time
+for i in range(3):
+    inpNum=int(input())
+    start=time.time()
+    print(printMonth(inpNum))
+    print((time.time()-start)*10000000)
+    start=time.time()
+    print((printMonth1(inpNum))*10000000)
+    print(time.time()-start)
+
+    
