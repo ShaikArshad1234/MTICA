c398fd6da88fbecf81e308985f974760fd13e663
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-30 03:22:16 -0800

    printMonth

---
+++ program06-printMonth.py
@@ -0,0 +1,33 @@
+def printMonth(dn):
+    mn=''
+    if(dn==1):
+        mn="January"
+    elif(dn==2):
+        mn= "February"
+    elif(dn==3):
+        mn="march"
+    elif(dn==4):
+        mn= "April"
+    elif(dn==5):
+        mn= "May"
+    elif(dn==6):
+        mn= "June"
+    elif(dn==7):
+        mn= "July"
+    elif(dn==8):
+        mn= "August"
+    elif(dn==9):
+        mn= "September"
+    elif(dn==10):
+        mn= "October"
+    elif(dn==11):
+        mn= "November"
+    elif(dn==12):
+        mn="December"
+    else:
+        mn= "Invalid"
+    return mn
+
+for i in range(3):
+    inpNum=int(input())
+    print(printMonth(inpNum))
