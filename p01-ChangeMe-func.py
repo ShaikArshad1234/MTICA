c64d71135dff06831b8648f7abdf1f41e7ddd1b5
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-31 03:06:39 -0800

    Functions

---
+++ p01-ChangeMe-func.py
@@ -0,0 +1,17 @@
+##def printMe(str1,n):
+##    n[-2]='Arshad'
+##    print(str1,n)
+##    return
+##
+##x=['Prathap','Arun']
+##printMe("Wakeup",x)
+##print("x:",x)
+
+def changeMe(lstn):
+    lstn=['Prathap','Arun','Prathap']
+    print(lstn)
+    return 
+lst=[1,4,11,23]
+print(changeMe(lst))
+print('lst',lst)
+
