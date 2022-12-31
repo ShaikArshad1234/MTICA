eb1ac50772ebdfbf697088da66f10dd20c87df5b
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-31 03:49:05 -0800

    database connection

--- connect-database1.py
+++ connect-database1.py
@@ -4,10 +4,6 @@ con=lite.connect('mtica.db')
 cur=con.cursor()
 cur.execute("SELECT * FROM cars")
 rows=cur.fetchall()
-##for row in rows:
-##    print(row)
-
 for row in rows:
-    print("{0:<3}|{1:<15}|{2:>5}".format(row[0],row[1],row[2]))
+    print("{0:<5}|{1:<10}|{2:<15}".format(row[0],row[1],row[2]))
     
-                                                       
