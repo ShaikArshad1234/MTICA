b6fe3012ca7e154383b03101f8679c8f6baaf35a
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-31 03:07:25 -0800

    FunctionsAdd

---
+++ p01-FuncAdd.py
@@ -0,0 +1,12 @@
+'''
+This is module covers 10th day in python training of mca
+'''
+def funAdd(x,y):
+    '''
+This  function is takes two integer arguments and returns sum of it.
+'''
+    temp=x+y
+    return temp
+Num1=int(input())
+Num2=int(input())
+print(funAdd(Num1,Num2))
