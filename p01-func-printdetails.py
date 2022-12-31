529f6f535cec6df4d5e5c43b6616aae9b9b793ea
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-31 03:08:32 -0800

    FunctionsPrintDetails

---
+++ p01-func-printdetails.py
@@ -0,0 +1,19 @@
+##def printDetails(name,marks=80):
+##    print("name:",name)
+##    print("marks:",marks)
+##    return None
+###printDetails()    #2 missing positional parameters 
+###printDetails('Arshad')
+##printDetails('Arshad',77)
+##printDetails(77,'Arshad')
+##printDetails(marks=84,name='Arshad')
+
+
+
+def printDetails(name,marks=80,age=22):
+    print("name:",name)
+    print("marks:",marks)
+    print("age:",age)
+    return None
+
+printDetails('Arshad')
