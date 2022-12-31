2b39c0d944adaf09d6fc4239658750ab889e3a12
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2022-12-31 03:04:18 -0800

    File Handling

---
+++ file Handling01.py
@@ -0,0 +1,7 @@
+fo=open(r"C:\Users\User\Desktop\Pythonpractice04\Day09\abc2.txt","w+")
+inpstr=input('Enter text:')
+fo.write(inpstr)
+inpstr=input('Enter text:')
+fo.write(inpstr)
+fo.close()
+print('Text written to file')
