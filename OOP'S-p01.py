8fc1bcdbf371e93f5ee018017ae9f74df0efb177
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2023-01-03 03:19:02 -0800

    Add files via upload

---
+++ OOP'S-p01.py
@@ -0,0 +1,18 @@
+class cat:
+    def __init__(self,color,legs):
+        self.color=color
+        self.legs=legs
+    def __str__(self):
+        temp="cat is "+self.color +' color' +'and has ' +str(self.legs)+' legs '
+        return temp
+if __name__=="__main__":
+    pet1=cat("Ginger",4)
+    print(pet1.legs)
+    print(pet1.color)
+    print(pet1)
+
+    pet2=cat("Brown",3)
+    print(pet2.legs)
+    print(pet2.color)
+    print(pet2)
+         
