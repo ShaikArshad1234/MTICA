fa7b6c58d3a5b54d179ff29bd802fccb8a772470
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2023-01-03 03:13:47 -0800

    Add files via upload

---
+++ printPattern3.py
@@ -0,0 +1,12 @@
+def printS(n):
+    
+    for i in range(1,n+1):
+        num=1
+        print()
+        for j in range(i):
+            print(num,end='')
+            num+=1
+            
+    return None
+inpNum=int(input())
+printS(inpNum)
