418260c85d1affde5efc6fbc244bbb595da458e2
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2023-01-03 03:35:06 -0800

    Add files via upload

---
+++ Disjoint with intersection.py
@@ -0,0 +1,8 @@
+set1={10,20,30,40,50}
+set2={60,70,10,80,90}
+
+if set1.isdisjoint(set2):
+    print("Two sets have no items in common")
+else:
+    print("two sets have items in common")
+    print(set1.intersection(set2))
