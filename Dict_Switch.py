faf6f2a06eacb5f2d0c59ef62e6dce555fd2dc70
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2023-01-04 03:21:34 -0800

    Add files via upload

---
+++ Dict_Switch.py
@@ -0,0 +1,25 @@
+
+def printBlue():
+    print('You choose blue!\n')
+    return
+def printRed():
+    print('You choose Red!\n')
+def printOrange():
+    print('You choose Orange!\n')
+def printYellow():
+    print('You choose Yellow!\n')
+def choice():
+    print('0:Blue')
+    print('1:Red')
+    print('2:Orange')
+    print('3:Yellow')
+    print('4:Quit')
+    return
+colorSelect={0:printBlue,1:printRed,2:printOrange,3:printYellow}
+selection=0
+while True:
+    if selection==4:break
+    choice()
+    selection=int(input("select a color option:"))
+    if (selection>=0) and (selection<4):
+        colorSelect[selection]()
