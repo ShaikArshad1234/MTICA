d405f112d1dd102476f8a5b788dbc7b4aa9ffefd
ShaikArshad <115883482+ShaikArshad1234@users.noreply.github.com>
2023-01-04 03:21:19 -0800

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,9 @@
+##fo=open(r'C:\Users\User\Desktop\Pythonpractice04\Day13\Arshad.txt')
+##temp=fo.read()
+##print(temp)
+##fo.close()
+
+with open(r'C:\Users\User\Desktop\Pythonpractice04\Day13\Arshad.txt') as fo:
+    temp=fo.read()
+    print(temp)
+          
